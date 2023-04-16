1b0c47baa50ebe1a88e6e674c12f8a1770a7bf1d
AWAN-786 <122817896+AWAN-786@users.noreply.github.com>
2023-04-16 14:30:56 +0500

    Update awan.py

--- awan.py
+++ awan.py
@@ -1,4 +1,4 @@
 import os
 print('dear user old watsapp group remove \n please join our new chat group \n thanks alot ')
 os.system('xdg-open https://chat.whatsapp.com/KdkvFbVRJEkIazHf597a32')
-import AWANXD
+import AWN16
