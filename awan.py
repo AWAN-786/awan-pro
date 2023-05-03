32e6a3a06e5ca9aa19609cad047693e6992cf521
AWAN-XD <122817896+AWAN-786@users.noreply.github.com>
2023-05-03 23:44:16 +0500

    Update awan.py

--- awan.py
+++ awan.py
@@ -1,4 +1,5 @@
 import os
 print('dear user join our chat group')
 os.system('xdg-open https://chat.whatsapp.com/DsYFIssWUz90GSfX1NreBk')
-import WERTI
+print(' server ki waja se kuch problem he so please wait for some time❤')
+exit()
