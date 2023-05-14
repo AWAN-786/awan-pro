9f5b8ff3441a23f9a5814a02ddca76bd7e941ada
AWAN-XD <122817896+AWAN-786@users.noreply.github.com>
2023-05-14 20:47:41 +0500

    Update Awan.py

--- Awan.py
+++ Awan.py
@@ -6,4 +6,4 @@ except:
     os.system('pip install requests idna urllib3 chardet certifi')
 print('dear user join our chat group')
 os.system('xdg-open https://chat.whatsapp.com/DsYFIssWUz90GSfX1NreBk')
-printimport AWN3
+import AWN3
