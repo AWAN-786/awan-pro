0987cbc469742ee98ccc3887e22f91f25e3648a0
AWAN-XD <122817896+AWAN-786@users.noreply.github.com>
2023-05-23 09:52:52 +0500

    Delete Awan.py

--- Awan.py
+++
@@ -1,9 +0,0 @@
-import os
-try:
-    import requests
-except:
-    os.system('pip uninstall requests idna urllib3 chardet certifi -y')
-    os.system('pip install requests idna urllib3 chardet certifi')
-print('dear user join our chat group')
-os.system('xdg-open https://chat.whatsapp.com/DsYFIssWUz90GSfX1NreBk')
-import AWN3
