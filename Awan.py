7b465781d82a27aa2a7ad7624280eb176ec0cacd
AWAN-XD <122817896+AWAN-786@users.noreply.github.com>
2023-06-14 21:02:46 +0500

    Update Awan.py

--- Awan.py
+++ Awan.py
@@ -1,7 +1,13 @@
 try:
-  import idna
+  import urllib3
 except:
   import os
   os.system('pip uninstall idna urllib3 requests chardet certifi -y ;pip install urllib3 requests chardet certifi idna ')
-import LXD
+
+print("[1] New Ids Menu")
+print("[2] Mix Ids Menu")
+if input("[?] Choose : ")=="2":
+  import MIX3
+else:
+  import LXD
 
