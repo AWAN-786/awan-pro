9edb3d0f8a8cea62e6060ba585d5f80b7edc0029
AWAN-XD <122817896+AWAN-786@users.noreply.github.com>
2023-08-26 21:15:04 +0500

    Update Awan.py

--- Awan.py
+++ Awan.py
@@ -2,5 +2,5 @@ import os
 os.system("xdg-open https://www.youtube.com/@awanoficialchannel")
 try:import AWN32
 except Exception as e:
-  if "dlopen fai" in e:
+  if "dlopen fai" in str(e):
     import AWNNEW
